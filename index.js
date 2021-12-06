77f4d02568dd1fd196827b150595719e300069ca
ВАШЕ_ИМЯAxelSFox <nimwenbadmod@gmail.com>
2021-12-06 15:06:58 +0300

    +1

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fistNum = process.argv[2];
 const secNum = process.argv[3];
 const colors = require('colors');
-const simpleNum = [];
+const simpleArr = [];
+let flag = "red";
+
 
 if (isNaN(fistNum) || isNaN(secNum)){
   console.log(colors.green("Нужно ввести два числа"));
@@ -15,16 +17,32 @@ for (let i = fistNum; i <= secNum; i++) {
     if (i % j == 0) continue nextNum; 
   }
   
-  simpleNum.push(i);
-} ;
-
-if (simpleNum.length == 0){
-  console.log(colors.red("Тут нет простых чисел"));
+  simpleArr.push(i);
 };
 
 
-console.log(simpleNum);
+if(simpleArr.length == 0){
+    console.log(colors.red("Простые числа не найдены"))
+}
+else {
+    simpleArr.map(function(item){
+    
+        switch (flag){
+        case "red":
+            console.log(colors.red(item));
+            flag = "yellow"; 
+            break; 
+        case "yellow":
+            console.log(colors.yellow(item));
+            flag = "green";  
+            break; 
+        case "green":
+            console.log(colors.green(item));
+            flag = "red";  
+            break; 
+    }
 
+})};
 
 
    
